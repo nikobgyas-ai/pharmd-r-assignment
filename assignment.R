4ea5387c813f280c084840c567369b95729556f1
nikobgyas-ai <nikobgyas@gmail.com>
2026-09-20 05:56:13 +0000

    Complete Assignment 1

---
+++ assignment.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+# PharmD Data Analysis - Assignment 1
+student_name <- "Nicholas"
+# Simple R objects
+age <- 25
+weight <- 70
+height <- 1.75
+bmi <- weight / (height^2)
+bmi
+# Vectors
+medicine_a <- c(10, 20, 30)
+medicine_b <- c(5, 10, 15)
+total <- medicine_a + medicine_b
+total
+seq(1, 10, by = 2)
+rep("pharmacy", 3)
